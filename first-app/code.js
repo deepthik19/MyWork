81c50237f8b6d6ec11176d9b788016344088b0a8
deepthik19 <deepthi198.katragadda@gmail.com>
2018-05-26 15:35:29 +0530

    nodejs

---
+++ first-app/code.js
@@ -0,0 +1,10 @@
+function sayHello(name){
+    console.log('Hello '+name);
+}
+
+sayHello('Deepthi');
+
+var logger = require('./logger');
+
+console.log(logger);
+logger.log('Hello');
---
+++ first-app/logger.js
@@ -0,0 +1,12 @@
+(function (exports, require, module, __filename, __dirname) {
+
+var url='https://www.unicode.com';
+
+function log(message){
+    //send an HTTP request
+    console.log(message);
+}
+
+module.exports.log = log;
+
+})
